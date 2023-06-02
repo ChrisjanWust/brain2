ecd454df9416da5c857469e765f819487d5ce17c
ChrisjanWust <chrisjanwust@gmail.com>
2023-06-03 01:17:19 +0200

    feature: django admin

--- brain2/chatbot/admin.py
+++ brain2/chatbot/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Account, Session, Context, Keyword
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("id", "created")
+    readonly_fields = ("id", "created")
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "account", "created")
+    readonly_fields = ("id", "created")
+
+
+@admin.register(Context)
+class ContextAdmin(admin.ModelAdmin):
+    list_display = ("id", "account", "text", "created")
+    readonly_fields = ("id", "created")
+
+
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ("word",)
+    filter_horizontal = ("contexts",)
